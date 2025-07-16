41c1bc3f712f51452f52f0bc08f7ac5f86f8f64e
Madirex <angel@madirex.com>
2025-07-16 11:54:11 +0200

    Creates a program that converts text to uppercase
    Implements a program that takes strings as arguments and converts them to uppercase.
    If no arguments are provided, the program outputs a specific message.

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anmateo- <anmateo-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/16 11:48:24 by anmateo-          #+#    #+#             */
+/*   Updated: 2025/07/16 11:48:24 by anmateo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <cctype>
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+
+	for (int i = 1; i < argc; i++)
+	{
+		std::string str = argv[i];
+		for (size_t j = 0; j < str.length(); j++)
+		{
+			std::cout << (char)std::toupper(str[j]);
+		}
+	}
+	std::cout << std::endl;
+	return (0);
+}
